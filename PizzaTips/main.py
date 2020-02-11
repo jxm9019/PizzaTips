f94e250b5545c2d54fb9e9e0efe9357b4a707094
Jake from State Farm <Jake from State Farm@DESKTOP-PL3F80H>
2020-02-11 14:59:43 -0500

    Initial Commit

---
+++ PizzaTips/main.py
@@ -0,0 +1,10 @@
+'''
+Created on Feb 11, 2020
+**PIZZA TIPS**
+App to collect tip data for analysis
+ 
+@author: Jake from State Farm
+'''
+
+if __name__ == '__main__':
+    print('Hello Pizza Tips')
